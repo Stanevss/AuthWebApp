907d15581035388b88e6d5c7623e61fd6ec1d99c
Stanislav <stanev.ss@yahooo.com>
2020-12-28 20:56:26 +0100

    AddedRoleProvider

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -60,7 +60,7 @@ namespace AuthWebApp.Controllers
                 {
                     using (UserContext db = new UserContext())
                     {
-                        db.Users.Add(new User { Email = model.Name, Password = model.Password, Age = model.Age });
+                        db.Users.Add(new User { Email = model.Name, Password = model.Password, Age = model.Age, RoleId = 2 });
                         db.SaveChanges();
 
                         user = db.Users.Where(u => u.Email == model.Name && u.Password == model.Password).FirstOrDefault();
--- Migrations/202012281759249_AddRoleToUser.cs
+++ Migrations/202012281759249_AddRoleToUser.cs
@@ -3,19 +3,16 @@
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class InitialCreate : DbMigration
+    public partial class AddRoleToUser : DbMigration
     {
         public override void Up()
         {
             CreateTable(
-                "dbo.Users",
+                "dbo.Roles",
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        Email = c.String(),
-                        Password = c.String(),
-                        Age = c.Int(nullable: false),
-
+                        Name = c.String(),
                     })
                 .PrimaryKey(t => t.Id);
             
@@ -23,7 +20,7 @@
         
         public override void Down()
         {
-            DropTable("dbo.Users");
+            DropTable("dbo.Roles");
         }
     }
 }
